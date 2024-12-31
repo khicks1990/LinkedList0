df52ad8c5c30ddd897695317216d931b382d97a3
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 06:30:45 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+    /**
+       The main method creates the linked list 
+       and invokes its print method.
+    */
+
+public class Main {
+    public static void main(String [] args)
+    {
+       LinkedList0 ll = new LinkedList0();
+     String str = "The contents of the list are:";
+     System.out.println(str);
+       ll.print();
+    }
+}
